1bed23d931b7f79783de8a246766eaa467bccc13
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-05-23 15:11:33 -0300

    Endpoint de la api

---
+++ app/api/route.ts
@@ -0,0 +1,47 @@
+import {NextResponse} from 'next/server'
+
+
+const DATA_SOURCE_URL = 'http://ws.audioscrobbler.com/2.0/';
+const artists = [
+    { artist: 'The+Beatles', album: 'Abbey+Road' },
+    { artist: 'Bob+Dylan', album: 'Blonde+On+Blonde' },
+    { artist: 'Jimi+Hendrix', album: 'Are+You+Experienced' },
+    { artist: 'Pink+Floyd', album: 'The+Dark+Side+of+the+Moon' },
+    { artist: 'Led+Zeppelin', album: 'Led+Zeppelin+IV' },
+    { artist: 'The+Beatles', album: 'Revolver' },
+    { artist: 'The+Rolling+Stones', album: 'Sticky+Fingers' },
+    { artist: 'The+Who', album: 'Who%27s+Next' },
+    { artist: 'The+Doors', album: 'The+Doors' },
+    { artist: 'Queen', album: 'A+Night+at+the+Opera' },
+    { artist: 'The+Velvet+Underground', album: 'The+Velvet+Underground+%26+Nico' },
+    { artist: 'David+Bowie', album: 'The+Rise+and+Fall+of+Ziggy+Stardust+and+the+Spiders+from+Mars' },
+    { artist: 'The+Ramones', album: 'Ramones' },
+    { artist: 'The+Police', album: 'Synchronicity' },
+    { artist: 'The+Smiths', album: 'The+Queen+Is+Dead' },
+    { artist: 'The+Cure', album: 'Disintegration' },
+    { artist: 'Joy+Division', album: 'Unknown+Pleasures' },
+    { artist: 'The+Clash', album: 'London+Calling' },
+];
+
+export async function GET() {
+
+    const apiKey = process.env.LASTFM_API_KEY;
+    console.log(apiKey)
+    const method = 'album.getInfo';
+    const fetchPromises = artists.map(({ artist, album }) => {
+        const url = `${DATA_SOURCE_URL}?method=${method}&artist=${artist}&album=${album}&api_key=${apiKey}&format=json`;
+        return fetch(url).then(response => response.json());
+    });
+
+    const results = await Promise.all(fetchPromises);
+
+
+    const transformedData = results.map(item => ({
+        albumName: item.album.name,
+        artist: item.album.artist,
+        imageUrl: item.album.image.find((img: { size: string; }) => img.size === 'large')?.['#text'] || '',
+    }));
+
+
+    return NextResponse.json(transformedData);
+}
