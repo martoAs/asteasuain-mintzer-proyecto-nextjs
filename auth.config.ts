f49133dbf365ec5668d4b42b2d5635f581447a87
martoAs <martinaasteasuain22@gmail.com>
2024-05-25 20:14:20 -0300

    no se

--- auth.config.ts
+++ auth.config.ts
@@ -8,11 +8,8 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnAdmin = nextUrl.pathname.startsWith('/admin');
-      if (isOnAdmin) {
-        if (isLoggedIn) return true;
+      if (isOnAdmin && !isLoggedIn) {
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/admin', nextUrl));
       }
       return true;
     },
