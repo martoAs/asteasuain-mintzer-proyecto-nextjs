b02c28c2206c4f0b91640077c5f8ea57bc76228d
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-05-28 15:11:56 -0300

    Cambiamos las cards

--- components/component/store/GridOfCards.tsx
+++ components/component/store/GridOfCards.tsx
@@ -2,11 +2,11 @@ import { StoreCards } from './StoreCards';
 
 export default function GridOfCards({ data }:{data: Data[]}) {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-20 ">
             {Array.from({ length: Math.ceil(data.length / 6) }, (_, i) => (
                 <div key={i} className="flex flex-wrap justify-center gap-4 mb-10"> {/* Added mb-10 for row separation */}
                     {Array.from({ length: 6 }, (_, j) => (
-                        <div key={j} className="flex-shrink-0 w-48 h-64 mb-10">
+                        <div key={j} className="flex-shrink-0 w-48 h-64 mb-20">
                             <StoreCards item={data[i * 6 + j]} />
                         </div>
                     ))}
--- components/component/store/StoreCards.tsx
+++ components/component/store/StoreCards.tsx
@@ -1,41 +1,64 @@
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import Image from "next/image";
-import Button from '@mui/material/Button';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import Chip from '@mui/joy/Chip';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import MarqueeOnOverflow from "@/components/component/store/MarqueeOnOverflow";
 
-export function StoreCards({ item }:{item: Data}) {
-    const icon = <ShoppingCartIcon />;
+export function StoreCards({item}: { item: Data }) {
     return (
-        <Card className="bg-[#31363F] text-gray-100 flex flex-col items-center">
-            <CardHeader className="w-full flex-col items-center">
-                <div className="w-24 h-24 ">
-                    <Image
-                        alt="Product"
-                        className="aspect-square object-cover rounded-t-lg"
-                        height="100"
+        <Card sx={{ width: 320, maxWidth: '100%', border: 'none'}} className="bg-[#EEEEEE]">
+            <CardOverflow>
+                <AspectRatio sx={{ minWidth: 100 }}>
+                    <img
                         src={item.imageUrl}
-                        width="100"
+                        srcSet= {`${item.imageUrl} 2x`}
+                        loading="lazy"
+                        alt=""
                     />
-                </div>
-            </CardHeader>
-            <CardContent className="flex flex-col items-center justify-center w-full text-center">
-                <MarqueeOnOverflow className="w-full flex flex-col items-center justify-center" direction="horizontal" animation={["animate-marqueeX", "animate-marqueeX2"]}>
-                    <span className="text-nowrap ">{item.artist}</span>
-                </MarqueeOnOverflow>
-                <MarqueeOnOverflow className="w-full flex flex-col items-center justify-center" direction="horizontal" animation={["animate-marqueeX", "animate-marqueeX2"]}>
-                     <span className="text-nowrap">{item.albumName}</span>
-                </MarqueeOnOverflow>
-            </CardContent>
-            <CardFooter className="w-full flex justify-center">
-                <Button
-                    className="w-full bg-[#76ABAE] text-[#FFFFFF] hover:bg-[#222831] dark:bg-[#3b2314] dark:text-gray-100 dark:hover:bg-[#2d1c10]"
-                    startIcon={icon}
+                </AspectRatio>
+            </CardOverflow>
+            <CardContent>
+                <Typography level="body-xs">{item.artist}</Typography>
+
+                    <Link
+                        href="#product-card"
+                        fontWeight="md"
+                        color="neutral"
+                        textColor="text.primary"
+                        overlay
+                        endDecorator={<ArrowOutwardIcon />}
+                    >
+                        <MarqueeOnOverflow className="w-full"
+                                           direction="horizontal"
+                                           animation={["animate-marqueeX", "animate-marqueeX2"]}>
+                            <span className="text-nowrap">{item.albumName}</span>
+                        </MarqueeOnOverflow>
+                    </Link>
+
+
+                <Typography
+                    level="title-lg"
+                    sx={{ mt: 1, fontWeight: 'xl' }}
+                    endDecorator={
+                        <Chip component="span" size="sm" variant="soft" color="success">
+                            Formato
+                        </Chip>
+                    }
                 >
-                    Add to Cart
+                    2,900 THB
+                </Typography>
+            </CardContent>
+            <CardOverflow>
+                <Button variant="solid" className="bg-[#59999C] hover:bg-[#5FC8CD]" size="lg">
+                    Add to cart
                 </Button>
-            </CardFooter>
+            </CardOverflow>
         </Card>
     );
 }
-
