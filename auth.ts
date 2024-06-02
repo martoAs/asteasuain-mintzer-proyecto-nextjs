e8a4365677ab79dc2ff93cbac5732264bb91df22
martoAs <martinaasteasuain22@gmail.com>
2024-06-02 14:53:43 -0300

    auth cambiada con prisma

--- auth.ts
+++ auth.ts
@@ -1,22 +1,34 @@
-
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
-import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcryptjs';
+import { prisma } from '@/lib/prisma';
  
-async function getUser(email: string): Promise<User | undefined> {
+async function getUser(email: string): Promise<User | null> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      return {
+        id: user.id.toString(),
+        name: user.name || '',
+        email: user.email,
+        password: user.password,
+      };
+    } else {
+      return null;
+    }
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
   }
 }
- 
+
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
