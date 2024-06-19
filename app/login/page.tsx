e5d824975bf633a8a4acbaa97c6e874d33355034
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-06-19 09:15:04 -0300

    Responsive para la ppagina de login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,12 +7,12 @@ export default async function LoginPage() {
   if(session)
     redirect("/admin")
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+    <div className=" w-full flex flex-col items-center justify-center min-h-screen relative">
+      <div className=" absolute">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
         </div>
-        <LoginForm />
+        <LoginForm/>
       </div>
-    </main>
+    </div>
   );
 }
