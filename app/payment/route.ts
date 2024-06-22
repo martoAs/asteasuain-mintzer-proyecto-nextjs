f615814301d8c1f188a134c0aff6d2110a0a4f2b
martoAs <martinaasteasuain22@gmail.com>
2024-06-22 11:22:15 -0300

    agrego order a la base de datos

--- app/payment/route.ts
+++ app/payment/route.ts
@@ -1,5 +1,6 @@
 import {NextRequest} from "next/server";
 import {MercadoPagoConfig, Payment} from "mercadopago";
+import {addOrder} from "@/components/component/admin/addOrder";
 
 
 const mp = new MercadoPagoConfig({
@@ -9,8 +10,15 @@ const mp = new MercadoPagoConfig({
 export async function POST(req: NextRequest){
     const body = await req.json().then((data) => data as {data: {id: string}});
     const payment = await new Payment(mp).get({id: body.data.id});
-    console.log("AL MENOS ENTRE");
-    console.log("SIGNATURE", req.headers.get("x-signature-id"));
-    console.log("PAYMENT", payment);
+    const order = {
+        id: payment.id,
+        message: payment.description,
+        amount: payment.transaction_amount,
+    };
+    if(order.id && order.message && order.amount){
+        await addOrder(order.id, order.message, order.amount)
+    }
+   
+
     return Response.json({success: true});
 }
