a127edd9d0b12c275560cc7332b1db65092c75f0
Thomas Maximiliano Mintzer <111892223+ThoMintz@users.noreply.github.com>
2024-06-23 11:50:07 -0300

    obtener producto en la api por id

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,21 +1,59 @@
 import {fetchAll} from "@/components/component/admin/fetchAll";
+import {NextRequest, NextResponse} from "next/server";
+import {fetchProductsFilteredForId} from "@/components/component/admin/fetchFilterId";
 
-export async function GET() {
-    const res = await fetchAll();
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
 
-    const data = res.map(item => ({
-        id: item.id,
-        title: item.title,
-        price: item.price,
-        new: item.new,
-        artist: item.artist,
-        formats: item.formats.map(format => ({
-            key: format.key,
-            format: format.format
-        }))
-    }));
+    if (id) {
+        try {
+            const res = await fetchProductsFilteredForId(Number(id));
+            if (!res) {
+                return new NextResponse('Producto no encontrado', { status: 404 });
+            }
 
-    return new Response(JSON.stringify({ data }), {
-        headers: { 'Content-Type': 'application/json' }
-    });
+            const item = res[0]
+
+            const data = {
+                id: item.id,
+                title: item.title,
+                price: item.price,
+                new: item.new,
+                artist: item.artist,
+                formats: item.formats.map(format => ({
+                    key: format.key,
+                    format: format.format
+                }))
+            };
+
+            return new NextResponse(JSON.stringify({ data }), {
+                headers: { 'Content-Type': 'application/json' }
+            });
+        } catch (error) {
+            return new NextResponse('Error al obtener el producto', { status: 500 });
+        }
+    } else {
+        try {
+            const res = await fetchAll();
+
+            const data = res.map(item => ({
+                id: item.id,
+                title: item.title,
+                price: item.price,
+                new: item.new,
+                artist: item.artist,
+                formats: item.formats.map(format => ({
+                    key: format.key,
+                    format: format.format
+                }))
+            }));
+
+            return new NextResponse(JSON.stringify({ data }), {
+                headers: { 'Content-Type': 'application/json' }
+            });
+        } catch (error) {
+            return new NextResponse('Error al obtener los productos', { status: 500 });
+        }
+    }
 }
