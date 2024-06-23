443b52711f11d2bcac95de92a534b796a0449373
martoAs <martinaasteasuain22@gmail.com>
2024-06-23 12:08:02 -0300

    volvi a ocultar el menu

--- components/component/admin/lastOrderComponent.tsx
+++ components/component/admin/lastOrderComponent.tsx
@@ -17,10 +17,12 @@ export default function LastOrderComponent({data, count}: PrincipalProps) {
     return (
         <div className="flex flex-col justify-center">
             <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-                <AdminMenu/>
+                <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
+                    <AdminMenu/>
+                </div>
                 <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
                     <div className="flex items-center">
-                        <h1 className="font-semibold text-lg md:text-2xl">Ultimas Ordenes</h1>
+                    <h1 className="font-semibold text-lg md:text-2xl">Ultimas Ordenes</h1>
                         <Link className="p-5" href="/admin">
                             <Button className="ml-auto" size="sm">
                                 Volver a productos
